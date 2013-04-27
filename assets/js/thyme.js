6e441b021368062dbca4a6b1d229ca3214aca50c
Sam Saccone <samccone@gmail.com>
2013-04-27 19:12:59 -0400

    add time data attr to each tr

--- assets/js/thyme.js
+++ assets/js/thyme.js
@@ -91,6 +91,7 @@
           if (dayOfWeek === day) {
             var d = document.createElement("span");
             d.innerHTML = days.shift();
+            cell.dataset.stamp = moment("" + (this.currentDate.year()) + "-" + (this.currentDate.month() + 1) + "-"+d.innerHTML).toDate().getTime();
             cell.appendChild(d);
           }
           row.appendChild(cell);
